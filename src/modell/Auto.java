100f47575eed1d088cf57798a41eb6b2989b1d02
Kalandor01 <rohovszkyakoska@gmail.com>
2023-02-13 13:52:09 +0100

    basic

---
+++ src/modell/Auto.java
@@ -0,0 +1,14 @@
+package modell;
+public class Auto extends Jarmu{
+    private boolean defekt;
+    
+    @Override
+    public boolean halad() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public void kereketCserel()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ src/modell/Jarmu.java
@@ -0,0 +1,26 @@
+package modell;
+public abstract class Jarmu {
+    private boolean beinditva;
+    private boolean uzemanyag;
+    private boolean megerkezett;
+    
+    public void beindit()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public void leallit()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public boolean tankol()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public boolean halad()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
